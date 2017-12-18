cff65114759d55a83d13425b3e2de0e1596bf932
吴浩麟 <wuhaolin@meituan.com>
2017-12-18 16:58:52 +0800

    更新域名

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -96,14 +96,14 @@ module.exports = {
     }),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'wuhaolin.cn');
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'resume.wuhaolin.cn');
 
       await publishGhPages();
 
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://wuhaolin.cn' // 这里注意改成你的在线简历的网站
+        'http://resume.wuhaolin.cn' // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
